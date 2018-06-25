f6619e80c832c384ca39bfb544622e4cd2da4bdd
Сергей <666programmer666@gmail.com>
2018-06-25 19:48:54 +0300

    Рабочий и актуальный blog.sql
    Рабочий и актуальный blog.sql.

--- blog.sql
+++ blog.sql
@@ -1,70 +1,47 @@
---
--- Скрипт сгенерирован Devart dbForge Studio for MySQL, Версия 7.3.131.0
--- Домашняя страница продукта: http://www.devart.com/ru/dbforge/mysql/studio
--- Дата скрипта: 05.05.2018 13:08:32
--- Версия сервера: 5.7.12-log
--- Версия клиента: 4.1
---
 
+CREATE DATABASE blog;
 
--- 
--- Отключение внешних ключей
--- 
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-
--- 
--- Установить режим SQL (SQL mode)
--- 
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-
---
--- Установка базы данных по умолчанию
---
 USE blog;
 
---
--- Удалить таблицу "news"
---
-DROP TABLE IF EXISTS news;
-
---
--- Установка базы данных по умолчанию
---
-USE blog;
-
---
--- Создать таблицу "news"
---
-CREATE TABLE news (
+CREATE TABLE posts (
   id int(11) NOT NULL AUTO_INCREMENT,
+  category_id int(11) NOT NULL,
   title varchar(128) NOT NULL,
+  prenews text NOT NULL,
+  body text NOT NULL,
   slug varchar(128) NOT NULL,
-  text text NOT NULL,
-  PRIMARY KEY (id),
-  INDEX slug (slug)
+  created_at datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (id)
 )
-ENGINE = INNODB
-AUTO_INCREMENT = 6
-AVG_ROW_LENGTH = 4096
-CHARACTER SET utf8
-COLLATE utf8_general_ci
-ROW_FORMAT = DYNAMIC;
+ENGINE = INNODB,
+AUTO_INCREMENT = 33,
+AVG_ROW_LENGTH = 2340,
+CHARACTER SET utf8,
+COLLATE utf8_general_ci;
 
--- 
--- Вывод данных для таблицы news
---
-INSERT INTO news VALUES
-(1, 'Hello!', 'hello', 'РџСЂРёРІРµС‚ РјРёСЂ!'),
-(2, 'РќРѕРІРѕСЃС‚СЊ 2 ', 'РќРѕРІРѕСЃС‚СЊ-2', 'РЅРѕРІРѕСЃС‚СЊ 2'),
-(3, 'РќРѕРІРѕСЃС‚СЊ 3', 'РќРѕРІРѕСЃС‚СЊ-3', 'РЅРѕРІРѕСЃС‚СЊ 3'),
-(4, 'РќРѕРІРѕСЃС‚СЊ 4', 'РќРѕРІРѕСЃС‚СЊ-4', 'РЅРѕРІРѕСЃС‚СЊ 4'),
-(5, 'РќРѕРІРѕСЃС‚СЊ 5', 'РќРѕРІРѕСЃС‚СЊ-5', 'РЅРѕРІРѕСЃС‚СЊ 5');
--- 
--- Восстановить предыдущий режим SQL (SQL mode)
--- 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+ALTER TABLE posts
+ADD INDEX slug (slug);
 
--- 
--- Включение внешних ключей
--- 
-/*!40014 SET FOREIGN_KEY_CHECKS = @OLD_FOREIGN_KEY_CHECKS */;
+CREATE TABLE categories (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  name varchar(255) NOT NULL,
+  created_at timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (id)
+)
+ENGINE = INNODB,
+AUTO_INCREMENT = 3,
+AVG_ROW_LENGTH = 8192,
+CHARACTER SET utf8,
+COLLATE utf8_general_ci;
+
+INSERT INTO posts VALUES
+
+(28, 2, 'test news 2', '', 'test news 2<br>', 'test-news-2', '0000-00-00 00:00:00'),
+(29, 1, 'Open test', '', 'Open test <br>', 'Open-test', '0000-00-00 00:00:00'),
+(30, 1, 'Close test', '', 'Close test<br>', 'Close-test', '2018-06-23 15:24:49'),
+
+
+INSERT INTO categories VALUES
+(1, 'Business', '2018-06-22 08:28:58'),
+(2, 'Technology', '2018-06-22 08:29:30');
+
